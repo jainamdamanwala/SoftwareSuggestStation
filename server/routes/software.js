78005755a309dfd6ee0eb78a6272a3d6b3479d2e
shubh0070 <sjiyani@stevens.edu>
2021-12-08 23:31:38 +0530

    Implemented software search feature

--- server/routes/software.js
+++ server/routes/software.js
@@ -6,4 +6,7 @@ var softwareController = require('../data/software');
 router.get('/', softwareController.hasAuthorization,
     softwareController.listTopItems);
 
+router.get('/search', softwareController.hasAuthorization,
+    softwareController.searchSoftware);
+
 module.exports = router;
