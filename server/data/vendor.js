b2bbcdfd57028c285f76fab5db70d751d88a16bb
shubh0070 <sjiyani@stevens.edu>
2021-12-11 00:31:01 +0530

    Ability delete and edit sotware for vendor and some misc bug fixes

--- server/data/vendor.js
+++ server/data/vendor.js
@@ -1,4 +1,5 @@
 var Software = require('../models/software');
+var xss = require('xss')
 
 exports.listSoftwares = function (req, res) {
     Software.find().exec(function (error, softwares) {
@@ -18,18 +19,45 @@ exports.listSoftwares = function (req, res) {
 exports.createSoftware = function (req, res) {
     var newSoftware = new Software();
 
-    if (!(req.body.name && req.body.tag && req.body.details &&
+    if (!(req.body.name && req.body.catagory && req.body.details &&
         req.body.subscriptionPrice && req.body.developerName)) {
         return res.redirect('/vendor');
     }
 
     newSoftware.name = req.body.name;
-    newSoftware.tag = req.body.tag;
+    newSoftware.catagory = req.body.catagory;
     newSoftware.details = req.body.details;
     newSoftware.developerName = req.body.developerName;
     newSoftware.subscriptionPrice = req.body.subscriptionPrice;
 
     newSoftware.save(function (error) {
+        return res.redirect('/vendor');
+    });
+};
+
+exports.deleteSoftware = function (req, res) {
+    var id = xss(req.query.deleteSoftwareId);
+
+    Software.findOne({ _id: id }).remove(function (error) {
+        return res.redirect('/vendor');
+    });
+};
+
+exports.updateSoftwarae = function (req, res) {
+    var id = req.body.updateSoftwareId;
+
+    if (!(req.body.name && req.body.catagory && req.body.details &&
+        req.body.subscriptionPrice && req.body.developerName)) {
+        return res.redirect('/vendor');
+    }
+
+    Software.findOneAndUpdate({ _id: id }, {
+        name: req.body.name,
+        developerName: req.body.developerName,
+        subscriptionPrice: req.body.subscriptionPrice,
+        catagory: req.body.catagory,
+        details: req.body.details
+    }, function (errr, data) {
         return res.redirect('/vendor');
     });
 };
--- server/routes/vendor.js
+++ server/routes/vendor.js
@@ -9,4 +9,10 @@ router.get('/', vendorController.hasAuthorization,
 router.post('/', vendorController.hasAuthorization,
     vendorController.createSoftware);
 
+router.post('/edit', vendorController.hasAuthorization,
+    vendorController.updateSoftwarae);
+
+router.get('/delete', vendorController.hasAuthorization,
+    vendorController.deleteSoftware);
+
 module.exports = router;
